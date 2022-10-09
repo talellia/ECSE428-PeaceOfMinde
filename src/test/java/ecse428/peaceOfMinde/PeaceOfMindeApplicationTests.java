7c1ae848757152c637478cf771c6ca4a59ea9e14
BrunoMarcoux <bruno.marcoux@mail.mcgill.ca>
2022-10-09 13:11:49 -0400

    feat: intial maven project

---
+++ src/test/java/ecse428/peaceOfMinde/PeaceOfMindeApplicationTests.java
@@ -0,0 +1,13 @@
+package ecse428.peaceOfMinde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaceOfMindeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
