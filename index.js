69d3955ea4ae0676a755727387d3036c19ea2be6
atalel <46718279+talellia@users.noreply.github.com>
2022-10-26 13:17:42 -0400

    set up front end functional

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+// *******************************************************************
+// SETUP
+// *******************************************************************
+
+
+
+// import express module and set up port
 const express = require('express')
-const path = require('path')
+const app = express()
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Static files
+app.use(express.static('/frontend/public'))
+app.use('/css',express.static(__dirname + '/frontend/public/css'))
+app.use('/js',express.static(__dirname + '/frontend/public/js'))
+app.use('/img',express.static(__dirname + '/frontend/public/img'))
+
+// Set views
+app.set('views', './frontend/views')
+app.set('view engine', 'ejs')
+
+app.get('', (req,res) =>{
+  res.render('index')
+})
+app.get('/about', (req,res) =>{
+  res.render('about')
+})
+
+
+//listen on port 5000
+app.listen(PORT, () => console.info(`listening on port ${PORT}`))
