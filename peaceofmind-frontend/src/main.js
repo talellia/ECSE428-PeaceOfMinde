1c4f6a84f750430f236de4b6eebbe933d64aeb9a
catherine-caron <rajaa.bk0@gmail.com>
2022-10-29 19:50:30 -0400

    Frontend with Vue integrated

---
+++ peaceofmind-frontend/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import BootstrapVue from "bootstrap-vue"
+import App from './App'
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
